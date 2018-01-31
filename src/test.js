41fe82b3de9d7d06b11e31e6d3769d99744ee904
LLM <857381350@qq.com>
2018-01-31 14:29:37 +0800

    测试vscode git

---
+++ src/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: LLM 
+ * @Date: 2018-01-31 14:27:53 
+ * @Last Modified by: LLM
+ * @Last Modified time: 2018-01-31 14:28:43
+ */
+function name(params) {
+    alert('this a test!');
+}
